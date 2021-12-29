0f87e3b772b283332550747b563cdfe09ed90df8
Bob Verheij <77615482+BobVerheij@users.noreply.github.com>
2021-12-29 16:49:47 +0100

    Add trailing backslash

--- src/services/fetchApiData.ts
+++ src/services/fetchApiData.ts
@@ -1,7 +1,7 @@
 import { Fixture, Live } from "fpl-api";
 import { NewBootstrap } from "../types/Types";
 
-const url = "https://fpl-proxy.vercel.app/fpl";
+const url = "https://fpl-proxy.vercel.app/fpl/";
 
 export async function fetchBootstrap(): Promise<NewBootstrap> {
   const response = await fetchPublicEndpoint("bootstrap-static/");
